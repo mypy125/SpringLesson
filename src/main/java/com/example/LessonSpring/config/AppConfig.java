499097d6a5b0a8f2661905c1608b71ed3e67740c
mohito <mypywelcome@gmail.com>
2024-03-23 23:09:52 +0400

    hibernate_configure

--- src/main/java/com/example/LessonSpring/config/AppConfig.java
+++ src/main/java/com/example/LessonSpring/config/AppConfig.java
@@ -1,19 +1,34 @@
 package com.example.LessonSpring.config;
 
-import com.example.LessonSpring.repository.TaskRepository;
-import com.example.LessonSpring.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.util.Objects;
 
 @Configuration
+@RequiredArgsConstructor
+@PropertySource("classpath:database.properties")
 public class AppConfig {
+    private final Environment environment;
+
     @Bean
-    UserRepository userRepository(){
-        return new UserRepository();
+    public DataSource dataSource(){
+        DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
+        driverManagerDataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("driver")));
+        driverManagerDataSource.setUrl(environment.getProperty("url"));
+        driverManagerDataSource.setUsername(environment.getProperty("username"));
+        driverManagerDataSource.setPassword(environment.getProperty("password"));
+        return driverManagerDataSource;
     }
 
     @Bean
-    TaskRepository taskRepository(){
-        return new TaskRepository();
+    JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(dataSource());
     }
 }
